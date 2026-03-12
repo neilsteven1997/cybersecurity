118da258f7c2a62006af09f8fa293d1e719af8af
Neil Steven Rosaroso <49906562+neilsteven1997@users.noreply.github.com>
2026-03-12 10:16:23 +0300

    Add files via upload
    Uploaded Python files for _python-basics_tryhackme_2026-03-10

---
+++ writeups/projects/python-basics_tryhackme/read_python-basics_tryhackme_2026-03-10.py
@@ -0,0 +1,19 @@
+f = open("file_name", "r")
+print(f.read())
+
+"""
+f = open("demofile1.txt", "a") # Append to an existing file
+f.write("The file will include more text..")
+f.close()
+
+f = open("demofile2.txt", "w") # Creating and writing to a new file
+f.write("demofile2 file created, with this content in!")
+f.close()
+
+import datetime #Import Library
+current_time = datetime.datetime.now()
+print(current_time)
+
+
+
+"""
